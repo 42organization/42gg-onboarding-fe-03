bb122c6d6bd1b1bd5607f4cc790d040945ce7110
hyobin <hyobb109@gmail.com>
2023-07-26 19:33:49 +0900

    [Style] 유저 아톰 수정

--- gg03/src/app/atoms/userAtom.ts
+++ gg03/src/app/atoms/userAtom.ts
@@ -1,11 +1,17 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
+import User from '../types/User';
 
 const { persistAtom } = recoilPersist();
 
-const userState = atom<string>({
-  key: 'auth',
-  default: 'none',
+const userState = atom<User>({
+  key: 'userState',
+  default: {
+    id: '',
+    name: '',
+    auth: '',
+    todo: [],
+  },
   effects_UNSTABLE: [persistAtom],
 });
 
