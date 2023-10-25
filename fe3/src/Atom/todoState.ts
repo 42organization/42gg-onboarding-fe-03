47aca4156a0aeaad6faa1f254d9b88830612f45f
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2023-10-25 20:00:14 +0900

    m

---
+++ fe3/src/Atom/todoState.ts
@@ -0,0 +1,17 @@
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
+
+interface todoStateInterface {
+  todo: string[],
+  completed: boolean[],
+};
+
+const todoState = atom<todoStateInterface | null>({
+  key: 'todoList',
+  default: null,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export default todoState;
--- fe3/src/app/todoList/[id]/page.tsx
+++ fe3/src/app/todoList/[id]/page.tsx
@@ -1,7 +1,37 @@
-'user client'
+'use client'
+import { usetodoState } from "@/hooks/useTodoState";
+
+const url = 'http://localhost:4000/'; // 대상 URL
+let todo:string[] = [];
+let completed:boolean[] = [];
 
 export default function page({ params }: { params: { id: string }}) {
-    return (
-      <div> My Post: {params.id}</div>
+  const [todoList, setTodoList] = usetodoState();
+  
+  fetch(url + "todo")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // JSON 형태로 응답 데이터를 파싱
+      })
+      .then((data) => {
+        for (let i = 0; i < data.length; ++i) {
+          if (data[i].id === params.id) {
+            todo = data[i].todo;
+            completed = data[i].completed;
+            console.log(data[i]);
+            break;
+          }
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  return (
+      <div>
+        My Post: {params.id}
+        {todo}
+      </div>
     )
   }
